48b77562e436a7706b697a21aa44bcc616b7a0a2
Adam Holt <me@adamholt.co.uk>
2014-11-26 13:35:27 +0100

    Bucket policy mock

--- lib/fog/aws/requests/storage/put_bucket_policy.rb
+++ lib/fog/aws/requests/storage/put_bucket_policy.rb
@@ -20,6 +20,23 @@ module Fog
           })
         end
       end
+
+      class Mock
+        #FIXME: You can't actually use the credentials for anything elsewhere in Fog
+        #FIXME: Doesn't do any validation on the policy
+        def put_bucket_policy(bucket_name, policy)
+          if bucket = data[:buckets][bucket_name]
+            bucket[:policy] = policy
+
+            Excon::Response.new.tap do |response|
+              response.body = { 'RequestId' => Fog::AWS::Mock.request_id }
+              response.status = 200
+            end
+          else
+            raise Fog::AWS::IAM::NotFound.new("The bucket with name #{bucket_name} cannot be found.")
+          end
+        end
+      end
     end
   end
 end
