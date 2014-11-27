d8b52e5ab875712963f1e6d880e7596da576eeaf
Adam Holt <me@adamholt.co.uk>
2014-11-27 17:21:00 +0100

    Delete bucket policy mock

--- lib/fog/aws/requests/storage/delete_bucket_policy.rb
+++ lib/fog/aws/requests/storage/delete_bucket_policy.rb
@@ -21,6 +21,21 @@ module Fog
           })
         end
       end
+
+      class Mock
+        def delete_bucket_policy(bucket_name)
+          if bucket = data[:buckets][bucket_name]
+            bucket[:policy] = nil
+
+            Excon::Response.new.tap do |response|
+              response.body = { 'RequestId' => Fog::AWS::Mock.request_id }
+              response.status = 200
+            end
+          else
+            raise Fog::AWS::IAM::NotFound.new("The bucket with name #{bucket_name} cannot be found.")
+          end
+        end
+      end
     end
   end
 end
