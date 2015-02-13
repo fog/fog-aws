6eef6d15c3cc934115901ca68777afe403e64099
Josh Lane <jlane@engineyard.com>
2015-02-13 13:49:05 -0800

    call setup_credentials in rds mock

--- lib/fog/aws/rds.rb
+++ lib/fog/aws/rds.rb
@@ -126,11 +126,13 @@ module Fog
 
         def initialize(options={})
           @use_iam_profile = options[:use_iam_profile]
-          @region = options[:region] || 'us-east-1'
+          @region          = options[:region] || 'us-east-1'
 
           unless ['ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-central-1', 'eu-west-1', 'us-east-1', 'us-west-1', 'us-west-2', 'sa-east-1'].include?(@region)
             raise ArgumentError, "Unknown region: #{@region.inspect}"
           end
+
+          setup_credentials(options)
         end
 
         def data
