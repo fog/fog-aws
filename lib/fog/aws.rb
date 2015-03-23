9a69a1d425c34a3d6e06f3fc9b21aa0c7925f96d
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2015-03-22 22:59:49 -0300

    Using relative paths

--- lib/fog/aws.rb
+++ lib/fog/aws.rb
@@ -5,28 +5,28 @@ module Fog
   end
 end
 
-require 'fog/aws/core'
+require File.expand_path('../aws/core', __FILE__)
 
-require 'fog/aws/auto_scaling'
-require 'fog/aws/beanstalk'
-require 'fog/aws/cdn'
-require 'fog/aws/cloud_formation'
-require 'fog/aws/cloud_watch'
-require 'fog/aws/compute'
-require 'fog/aws/data_pipeline'
-require 'fog/aws/dns'
-require 'fog/aws/dynamodb'
-require 'fog/aws/elasticache'
-require 'fog/aws/elb'
-require 'fog/aws/emr'
-require 'fog/aws/federation'
-require 'fog/aws/glacier'
-require 'fog/aws/iam'
-require 'fog/aws/rds'
-require 'fog/aws/redshift'
-require 'fog/aws/ses'
-require 'fog/aws/simpledb'
-require 'fog/aws/sns'
-require 'fog/aws/sqs'
-require 'fog/aws/storage'
-require 'fog/aws/sts'
+require File.expand_path('../aws/auto_scaling', __FILE__)
+require File.expand_path('../aws/beanstalk', __FILE__)
+require File.expand_path('../aws/cdn', __FILE__)
+require File.expand_path('../aws/cloud_formation', __FILE__)
+require File.expand_path('../aws/cloud_watch', __FILE__)
+require File.expand_path('../aws/compute', __FILE__)
+require File.expand_path('../aws/data_pipeline', __FILE__)
+require File.expand_path('../aws/dns', __FILE__)
+require File.expand_path('../aws/dynamodb', __FILE__)
+require File.expand_path('../aws/elasticache', __FILE__)
+require File.expand_path('../aws/elb', __FILE__)
+require File.expand_path('../aws/emr', __FILE__)
+require File.expand_path('../aws/federation', __FILE__)
+require File.expand_path('../aws/glacier', __FILE__)
+require File.expand_path('../aws/iam', __FILE__)
+require File.expand_path('../aws/rds', __FILE__)
+require File.expand_path('../aws/redshift', __FILE__)
+require File.expand_path('../aws/ses', __FILE__)
+require File.expand_path('../aws/simpledb', __FILE__)
+require File.expand_path('../aws/sns', __FILE__)
+require File.expand_path('../aws/sqs', __FILE__)
+require File.expand_path('../aws/storage', __FILE__)
+require File.expand_path('../aws/sts', __FILE__)
