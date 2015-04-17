ec3eb24d981234a3c844e62b8f73edce9c67c49c
Eugene Howe <eugene@xtreme-computers.net>
2015-04-17 11:26:32 -0400

    mocks for topic permissions

--- tests/requests/sns/topic_tests.rb
+++ tests/requests/sns/topic_tests.rb
@@ -32,6 +32,14 @@ Shindo.tests('AWS::SNS | topic lifecycle tests', ['aws', 'sns']) do
       Fog::AWS[:sns].get_topic_attributes(@topic_arn).body
     end
 
+    tests("#add_permission('#{@topic_arn}')").formats(AWS::SNS::Formats::BASIC) do
+      Fog::AWS[:sns].add_permission('TopicArn' => @topic_arn, 'Label' => 'Test', 'ActionName.member.1' => 'Subscribe', 'AWSAccountId.member.1' => '1234567890').body
+    end
+
+    tests("#remove_permission('#{@topic_arn}')").formats(AWS::SNS::Formats::BASIC) do
+      Fog::AWS[:sns].remove_permission('TopicArn' => @topic_arn, 'Label' => 'Test').body
+    end
+
     tests("#delete_topic('#{@topic_arn}')").formats(AWS::SNS::Formats::BASIC) do
       Fog::AWS[:sns].delete_topic(@topic_arn).body
     end
