0dad896add1f95880a18b8fa696709135f1def64
Carlos Lima <carlos@cpan.org>
2015-06-29 02:58:59 +0800

    The :geo_location attribute needs to be xml formatted before calling aws
    Without this, calling `record.destroy` on a record that has geolocation
    set will cause an exception with:
    Fog::DNS::AWS::Error: MalformedInput => Unexpected complex element termination

--- tests/requests/dns/change_resource_record_sets_tests.rb
+++ tests/requests/dns/change_resource_record_sets_tests.rb
@@ -7,4 +7,23 @@ Shindo.tests('Fog::DNS[:aws] | change_resource_record_sets', ['aws', 'dns']) do
       zone_id == Fog::DNS::AWS.elb_hosted_zone_mapping['eu-west-1']
     end
   end
+  tests("#change_resource_record_sets_data formats geolocation properly") do
+    change_batch = [{
+        :action=>"CREATE",
+        :name=>"ark.m.example.net.",
+        :resource_records=>["1.1.1.1"],
+        :ttl=>"300",
+        :type=>"A",
+        :set_identifier=>"ark",
+        :geo_location=>{"CountryCode"=>"US", "SubdivisionCode"=>"AR"},
+        }]
+
+    result = Fog::DNS::AWS.change_resource_record_sets_data('zone_id123', change_batch)
+      .match(%r{<GeoLocation>.*</GeoLocation>})
+    returns("<GeoLocation><CountryCode>US</CountryCode><SubdivisionCode>AR</SubdivisionCode></GeoLocation>") {
+      result ? result[0] : ''
+    }
+
+    result
+  end
 end
