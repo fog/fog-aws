8aac0959f367a2ff1846702af39268f254f5a286
Michelle Noorali <michellemolu@gmail.com>
2015-08-13 15:47:21 -0700

    fix option group membership typo

--- lib/fog/aws/requests/rds/restore_db_instance_from_db_snapshot.rb
+++ lib/fog/aws/requests/rds/restore_db_instance_from_db_snapshot.rb
@@ -63,7 +63,7 @@ module Fog
             "LicenseModel"                     => options['LicenseModel'] || snapshot['LicenseModel'] || 'general-public-license',
             "MasterUsername"                   => options['MasterUsername'] || snapshot['MasterUsername'],
             "MultiAZ"                          => options['MultiAZ'],
-            "OptiongroupMemberships"           => option_group_membership,
+            "OptionGroupMemberships"           => option_group_membership,
             "PendingModifiedValues"            => { 'MasterUserPassword' => '****' }, # This clears when is available
             "PreferredBackupWindow"            => '08:00-08:30',
             "PreferredMaintenanceWindow"       => 'mon:04:30-mon:05:00',
