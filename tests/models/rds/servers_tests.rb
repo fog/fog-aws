c43d72c0442d49c9e98c394e6b420b3a2fd7cfa1
Michelle Noorali <michellemolu@gmail.com>
2015-08-31 18:26:56 -0700

    restore to pit test

--- tests/models/rds/servers_tests.rb
+++ tests/models/rds/servers_tests.rb
@@ -5,20 +5,25 @@ Shindo.tests("AWS::RDS | servers", ['aws', 'rds']) do
   end
 
   tests("#restore").succeeds do
-    instance = Fog::AWS[:rds].servers.create(rds_default_server_params.merge(:id => uniq_id("fog-snapshot-test")))
+    instance_id = uniq_id("fog-snapshot-test")
+    instance = Fog::AWS[:rds].servers.create(rds_default_server_params.merge(:id => instance_id))
+    instance.wait_for { ready? }
 
     snapshot_id = uniq_id('fog-snapshot-test')
     @snapshot = instance.snapshots.create(:id => snapshot_id )
     instance.destroy
 
     db_name = uniq_id('fog-db-name')
-    @restore_instance = Fog::AWS[:rds].servers.restore(snapshot_id, db_name, {'master_username' => instance.master_username, 'flavor_id' => 'db.m3.medium'})
+    @restored_from_snapshot_instance = Fog::AWS[:rds].servers.restore('source_snapshot_id' => snapshot_id, 'id' => db_name, 'flavor_id' => 'db.m3.medium')
+
+    db_name = uniq_id('fog-db-name')
+    @restored_from_pit_instance = Fog::AWS[:rds].servers.restore('use_latest_restorable_time' => true, 'id' => db_name, 'source_db_name' => instance.id, flavor_id => 'db.m3.medium')
   end
 
-  if Fog.mocking? && @restore_instance.respond_to?(:ready?)
-    @restore_instance.wait_for { ready? }
+  if Fog.mocking? && @restored_from_snapshot_instance.respond_to?(:ready?)
+    @restored_from_snapshot_instance.wait_for { ready? }
   end
 
   @snapshot.destroy
-  @restore_instance.destroy
+  @restored_from_snapshot_instance.destroy
 end
