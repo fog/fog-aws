84b406475a7b49179325a1b89e89001de2165739
David Vaz <david.ferd.vaz@gmail.com>
2016-07-01 21:42:59 +0530

    added mock for modify_db_snapshot_attribute request

--- lib/fog/aws/requests/rds/modify_db_snapshot_attribute.rb
+++ lib/fog/aws/requests/rds/modify_db_snapshot_attribute.rb
@@ -27,6 +27,22 @@ module Fog
           }.merge!(params))
         end
       end
+      class Mock
+        #
+        # Usage
+        #
+        # Fog::AWS[:rds].modify_db_snapshot_attribute("snap-identifier", {"Add.MemberId"=>"389480430104"})
+        #
+
+        def modify_db_snapshot_attribute(db_snapshot_identifier, attributes)
+          response = Excon::Response.new
+          response.status = 200
+          response.body = {
+            'requestId' => Fog::AWS::Mock.request_id
+          }.merge!(data)
+          response
+        end
+      end
     end
   end
 end
