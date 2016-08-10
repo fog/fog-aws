1999e61f4d4b5797d42843796f67cf7bfbae8722
Chanakya Devraj <chanakya.devraj@cuelogic.co.in>
2016-08-10 15:07:23 +0000

    added DBSubnetGroupName to DbClusterParser

--- lib/fog/aws/parsers/rds/db_cluster_parser.rb
+++ lib/fog/aws/parsers/rds/db_cluster_parser.rb
@@ -31,7 +31,7 @@ module Fog
 
           def end_element(name)
             case name
-            when 'Port', 'Engine', 'Status', 'BackupRetentionPeriod', 'DBSubnetGroup', 'EngineVersion', 'Endpoint', 'DBClusterParameterGroup', 'DBClusterIdentifier', 'PreferredBackupWindow', 'PreferredMaintenanceWindow', 'AllocatedStorage', 'MasterUsername'
+            when 'Port', 'Engine', 'Status', 'BackupRetentionPeriod', 'DBSubnetGroupName', 'EngineVersion', 'Endpoint', 'DBClusterParameterGroup', 'DBClusterIdentifier', 'PreferredBackupWindow', 'PreferredMaintenanceWindow', 'AllocatedStorage', 'MasterUsername'
               @db_cluster[name] = value
             when 'VpcSecurityGroups'
               @in_vpc_security_groups = false
