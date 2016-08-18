adb32276e8adf393d380d3994f7798493d7b5f60
Kevin Loiseau <k.loiseauand@gmail.com>
2016-08-18 17:18:20 +0200

    Feedback : enhance test

--- tests/models/compute/security_group_tests.rb
+++ tests/models/compute/security_group_tests.rb
@@ -38,13 +38,25 @@ Shindo.tests("Fog::Compute[:aws] | security_group", ['aws']) do
     test("authorize access at a port range (egress rule)") do
       @group.authorize_port_range(5000..6000, :direction => 'egress')
       @group.reload
-      @group.ip_permissions_egress.size == 1
+      ip_permission_egress = @group.ip_permissions_egress.find do |permission|
+        permission['fromPort'] == 5000 &&
+          permission['toPort'] == 6000 &&
+          permission['ipProtocol'] == 'tcp' &&
+          permission['ipRanges'] == [{ 'cidrIp' => '0.0.0.0/0' }]
+      end
+      !ip_permission_egress.nil?
     end
 
     test("revoke access at a port range (egress rule)") do
       @group.revoke_port_range(5000..6000, :direction => 'egress')
       @group.reload
-      @group.ip_permissions_egress.empty?
+      ip_permission_egress = @group.ip_permissions_egress.find do |permission|
+        permission['fromPort'] == 5000 &&
+          permission['toPort'] == 6000 &&
+          permission['ipProtocol'] == 'tcp' &&
+          permission['ipRanges'] == [{ 'cidrIp' => '0.0.0.0/0' }]
+      end
+      ip_permission_egress.nil?
     end
 
     group_forms = [
