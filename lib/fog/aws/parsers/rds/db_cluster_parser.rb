619efda0eab16318b19a1c7b92711f8500814a2f
Chanakya Devraj <chanakyadevraj@yahoo.com>
2016-08-24 12:52:23 +0530

    indented

--- lib/fog/aws/parsers/rds/db_cluster_parser.rb
+++ lib/fog/aws/parsers/rds/db_cluster_parser.rb
@@ -34,7 +34,7 @@ module Fog
             when 'Port', 'Engine', 'Status', 'BackupRetentionPeriod', 'EngineVersion', 'Endpoint', 'DBClusterParameterGroup', 'DBClusterIdentifier', 'PreferredBackupWindow', 'PreferredMaintenanceWindow', 'AllocatedStorage', 'MasterUsername'
               @db_cluster[name] = value
             when 'DBSubnetGroup'
-          		@db_cluster['DBSubnetGroupName'] = value
+              @db_cluster['DBSubnetGroupName'] = value
             when 'VpcSecurityGroups'
               @in_vpc_security_groups = false
               @db_cluster['VpcSecurityGroups'] = @vpc_security_groups
