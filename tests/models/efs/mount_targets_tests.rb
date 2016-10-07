fd750c1205ac2ae8d5d1302c0ddf15633c10a615
Eugene Howe <eugene@xtreme-computers.net>
2016-10-07 09:45:49 -0400

    remove debugging code

--- tests/models/efs/mount_targets_tests.rb
+++ tests/models/efs/mount_targets_tests.rb
@@ -1,10 +1,5 @@
 Shindo.tests("AWS::EFS | mount targets", ["aws", "efs"]) do
-  if ENV['FILE_SYSTEM_ID']
-    @file_system = Fog::AWS[:efs].file_systems.get(ENV["FILE_SYSTEM_ID"])
-  else
-    @file_system = Fog::AWS[:efs].file_systems.create(:creation_token => "fogtoken#{rand(999).to_s}")
-  end
-
+  @file_system = Fog::AWS[:efs].file_systems.create(:creation_token => "fogtoken#{rand(999).to_s}")
   @file_system.wait_for { ready? }
 
   mount_target_params = {
@@ -14,7 +9,5 @@ Shindo.tests("AWS::EFS | mount targets", ["aws", "efs"]) do
 
   collection_tests(Fog::AWS[:efs].mount_targets(:file_system_id => @file_system.identity), mount_target_params, true)
 
-  unless ENV["LEAVE_FILE_SYSTEM"] = 'true'
-    @file_system.destroy
-  end
+  @file_system.destroy
 end
