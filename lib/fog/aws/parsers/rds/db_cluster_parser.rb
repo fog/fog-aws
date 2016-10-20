5835c4c09d8c99f9a7fcd52abac9a20446cbf3c9
Chanakya Devraj <chanakyadevraj@gmail.com>
2016-10-20 18:02:22 +0530

    Update db_cluster_parser.rb

--- lib/fog/aws/parsers/rds/db_cluster_parser.rb
+++ lib/fog/aws/parsers/rds/db_cluster_parser.rb
@@ -31,10 +31,8 @@ module Fog
 
           def end_element(name)
             case name
-            when 'Port', 'Engine', 'Status', 'BackupRetentionPeriod', 'EngineVersion', 'Endpoint', 'DBClusterParameterGroup', 'DBClusterIdentifier', 'PreferredBackupWindow', 'PreferredMaintenanceWindow', 'AllocatedStorage', 'MasterUsername'
+            when 'Port', 'Engine', 'Status', 'BackupRetentionPeriod', 'DBSubnetGroup', 'EngineVersion', 'Endpoint', 'DBClusterParameterGroup', 'DBClusterIdentifier', 'PreferredBackupWindow', 'PreferredMaintenanceWindow', 'AllocatedStorage', 'MasterUsername'
               @db_cluster[name] = value
-            when 'DBSubnetGroup'
-              @db_cluster['DBSubnetGroupName'] = value
             when 'VpcSecurityGroups'
               @in_vpc_security_groups = false
               @db_cluster['VpcSecurityGroups'] = @vpc_security_groups
