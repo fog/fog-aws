10bf21157b1badf89273248d4abf5ee6abd17f24
Yusuke Ebihara <biibiebi1234@gmail.com>
2016-11-19 12:47:15 +0900

    Fix the bug to show the warning deprecated usage of the Code Climate Test Reporter when running tests.

--- tests/helper.rb
+++ tests/helper.rb
@@ -1,6 +1,6 @@
 begin
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
+  require 'simplecov'
+  SimpleCov.start
 rescue LoadError => e
   $stderr.puts "not recording test coverage: #{e.inspect}"
 end
