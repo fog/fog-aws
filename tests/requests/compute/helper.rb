66187d73fb015055910baf651a4d4c6f8c00d1ca
Eddie Johnston <eddie@beanstalk.ie>
2017-07-06 16:33:53 +0100

    Fix Fog::Compute::AWS::Images#all
    Calling #all fails with the error: `Excon::Error::Socket: 25:28: FATAL: Document is empty (Nokogiri::XML::SyntaxError)`. This was because of the parser not handling nil creationDate values:
    
    ```
    when 'creationDate'
      @image[name] = Time.parse(value)
    ```
    
    This commit ensures nil `creationDate` fields are not parsed.

--- tests/requests/compute/helper.rb
+++ tests/requests/compute/helper.rb
@@ -2,9 +2,26 @@ class AWS
   module Compute
     module Formats
       BASIC = {
-        'requestId' => String,
-        'return'    => ::Fog::Boolean
+        'requestId' => String
       }
+
+      DESCRIBE_IMAGES = BASIC.merge({
+        "imagesSet" => [{
+          "imageId" => String,
+          "imageLocation" => String,
+          "imageState" => String,
+          "imageOwnerId" => String,
+          "creationDate" => Fog::Nullable::String,
+          "isPublic" => Fog::Nullable::Boolean,
+          "architecture" => String,
+          "imageType" => String,
+          "imageOwnerAlias" => String,
+          "rootDeviceType" => String,
+          "blockDeviceMapping" => Array,
+          "virtualizationType" => String,
+          "hypervisor" => String
+        }]
+      })
     end
   end
 end
