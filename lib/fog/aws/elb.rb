f9d82d4e3b505451930e270c7458f5bc60334407
Ross Andrews <ross.andrews@gmail.com>
2018-01-05 15:53:06 -0600

    Added to recognized params

--- lib/fog/aws/elb.rb
+++ lib/fog/aws/elb.rb
@@ -14,7 +14,7 @@ module Fog
       class ValidationError             < Fog::Errors::Error; end
 
       requires :aws_access_key_id, :aws_secret_access_key
-      recognizes :region, :host, :path, :port, :scheme, :persistent, :use_iam_profile, :aws_session_token, :aws_credentials_expire_at, :instrumentor, :instrumentor_name
+      recognizes :region, :host, :path, :port, :scheme, :persistent, :use_iam_profile, :aws_session_token, :aws_credentials_expire_at, :instrumentor, :instrumentor_name, :version
 
       request_path 'fog/aws/requests/elb'
       request :configure_health_check
