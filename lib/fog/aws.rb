eb204b0396548dca072bff26bb74a71c017fab8a
geemus <geemus@gmail.com>
2018-06-25 08:40:31 -0500

    add missing regions

--- lib/fog/aws.rb
+++ lib/fog/aws.rb
@@ -230,11 +230,12 @@ module Fog
 
     def self.regions
       @regions ||= [
-        'ap-northeast-1', 'ap-northeast-2',
+        'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3',
         'ap-south-1',
         'ap-southeast-1', 'ap-southeast-2',
         'ca-central-1',
         'cn-north-1',
+        'cn-northwest-1',
         'eu-central-1',
         'eu-west-1', 'eu-west-2', 'eu-west-3',
         'us-east-1', 'us-east-2',
