8ba653a6f5067fc8c4f78599f8638b26b54e806d
KevinLoiseau <k.loiseauand@gmail.com>
2019-08-08 11:14:29 +0200

    Fix subnet's parsings

--- lib/fog/aws/parsers/compute/describe_subnets.rb
+++ lib/fog/aws/parsers/compute/describe_subnets.rb
@@ -7,6 +7,10 @@ module Fog
             @subnet = { 'tagSet' => {} }
             @response = { 'subnetSet' => [] }
             @tag = {}
+            @ipv6_cidr_block_association = {}
+            @in_tag_set = false
+            @in_ipv6_cidr_block_association_set = false
+            @in_cidr_block_state = false
           end
 
           def start_element(name, attrs = [])
@@ -14,19 +18,42 @@ module Fog
             case name
             when 'tagSet'
               @in_tag_set = true
+            when 'ipv6CidrBlockAssociationSet'
+              @in_ipv6_cidr_block_association_set = true
+            when 'ipv6CidrBlockState'
+              @in_cidr_block_state = true
             end
           end
 
           def end_element(name)
             if @in_tag_set
               case name
+              when 'item'
+                @subnet['tagSet'][@tag['key']] = @tag['value']
+                @tag = {}
+              when 'key', 'value'
+                @tag[name] = value
+              when 'tagSet'
+                @in_tag_set = false
+              end
+            elsif @in_ipv6_cidr_block_association_set
+              if @in_cidr_block_state
+                case name
+                when 'state'
+                  @ipv6_cidr_block_association['ipv6CidrBlockState'] = { name => value }
+                when 'ipv6CidrBlockState'
+                  @in_cidr_block_state = false
+                end
+              else
+                case name
                 when 'item'
-                  @subnet['tagSet'][@tag['key']] = @tag['value']
-                  @tag = {}
-                when 'key', 'value'
-                  @tag[name] = value
-                when 'tagSet'
-                  @in_tag_set = false
+                  @subnet['ipv6CidrBlockAssociationSet'] = @ipv6_cidr_block_association
+                  @ipv6_cidr_block_association = {}
+                when 'ipv6CidrBlock', 'associationId'
+                  @ipv6_cidr_block_association[name] = value
+                when 'ipv6CidrBlockAssociationSet'
+                  @in_ipv6_cidr_block_association_set = false
+                end
               end
             else
               case name
