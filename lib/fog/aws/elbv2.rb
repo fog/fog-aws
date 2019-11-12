893ee802da44ca241cd58b3cd86ebf7b63cacfff
KevinLoiseau <k.loiseauand@gmail.com>
2019-11-12 14:57:13 +0100

    Implement ELBV2 add_tags endpoint

--- lib/fog/aws/elbv2.rb
+++ lib/fog/aws/elbv2.rb
@@ -2,6 +2,7 @@ module Fog
   module AWS
     class ELBV2 < ELB
       request_path 'fog/aws/requests/elbv2'
+      request :add_tags
       request :describe_load_balancers
       request :describe_listeners
 
