4ef5513890da313fc3db293b2438d7962169b88d
KevinLoiseau <k.loiseauand@gmail.com>
2019-11-26 11:18:14 +0100

    Take care of Certificates block in listeners parser

--- tests/parsers/elbv2/describe_listeners_tests.rb
+++ tests/parsers/elbv2/describe_listeners_tests.rb
@@ -7,7 +7,7 @@ DESCRIBE_LISTENERS_RESULT = <<-EOF
     <Listeners>
       <member>
         <LoadBalancerArn>arn:aws:elasticloadbalancing:us-west-2:123456789012:loadbalancer/app/my-load-balancer/50dc6c495c0c9188</LoadBalancerArn>
-        <Protocol>HTTP</Protocol>
+        <Protocol>HTTPS</Protocol>
         <Port>80</Port>
         <ListenerArn>arn:aws:elasticloadbalancing:us-west-2:123456789012:listener/app/my-load-balancer/50dc6c495c0c9188/f2f7dc8efc522ab2</ListenerArn>
         <DefaultActions>
@@ -16,6 +16,11 @@ DESCRIBE_LISTENERS_RESULT = <<-EOF
             <TargetGroupArn>arn:aws:elasticloadbalancing:us-west-2:123456789012:targetgroup/my-targets/73e2d6bc24d8a067</TargetGroupArn>
           </member>
         </DefaultActions>
+        <Certificates>
+          <member>
+            <CertificateArn>arn:aws:elasticloadbalancing:us-west-2:123456789012:certificate/56d36256-1245-40d6-916e-6f5a95e2b4c6</CertificateArn>
+          </member>
+        </Certificates>
       </member>
     </Listeners>
   </DescribeListenersResult>
