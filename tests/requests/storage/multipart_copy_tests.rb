e75a4bac0f1335613e403213485c04603d8fccf1
Stan Hu <stanhu@gmail.com>
2020-11-18 07:49:46 -0800

    Add test for copying empty object

--- tests/requests/storage/multipart_copy_tests.rb
+++ tests/requests/storage/multipart_copy_tests.rb
@@ -4,6 +4,20 @@ Shindo.tests('Fog::Storage[:aws] | copy requests', ["aws"]) do
 
   @directory = Fog::Storage[:aws].directories.create(:key => uniq_id('fogmultipartcopytests'))
 
+  tests('copies an empty object') do
+    Fog::Storage[:aws].put_object(@directory.identity, 'empty_object', '')
+
+    file = Fog::Storage[:aws].directories.new(key: @directory.identity).files.get('empty_object')
+    file.multipart_chunk_size = Fog::AWS::Storage::File::MIN_MULTIPART_CHUNK_SIZE
+
+    tests("#copy_object('#{@directory.identity}', 'empty_copied_object'").succeeds do
+      file.copy(@directory.identity, 'empty_copied_object')
+    end
+
+    copied = Fog::Storage[:aws].directories.new(key: @directory.identity).files.get('empty_copied_object')
+    test("copied is the same") { copied.body == file.body }
+  end
+
   tests('copies a small object') do
     Fog::Storage[:aws].put_object(@directory.identity, 'fog_object', lorem_file)
 
