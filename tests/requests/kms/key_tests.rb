3f89d37a02567fe378b229534b4aa58521f682d4
geemus <geemus@gmail.com>
2024-12-09 15:51:36 +0000

    add kms test lint fixes

--- tests/requests/kms/key_tests.rb
+++ tests/requests/kms/key_tests.rb
@@ -1,23 +1,22 @@
-Shindo.tests('AWS::KMS | key requests', ['aws', 'kms']) do
+Shindo.tests('AWS::KMS | key requests', %w[aws kms]) do
   key_id = nil
 
   tests('success') do
-    tests("#create_key").data_matches_schema(AWS::KMS::Formats::DESCRIBE_KEY) do
+    tests('#create_key').data_matches_schema(AWS::KMS::Formats::DESCRIBE_KEY) do
       result = Fog::AWS[:kms].create_key.body
-      key_id = result["KeyMetadata"]["KeyId"]
+      key_id = result['KeyMetadata']['KeyId']
 
       result
     end
   end
 
-  tests("#describe_key").data_matches_schema(AWS::KMS::Formats::DESCRIBE_KEY) do
+  tests('#describe_key').data_matches_schema(AWS::KMS::Formats::DESCRIBE_KEY) do
     result = Fog::AWS[:kms].describe_key(key_id).body
     returns(key_id) { result['KeyMetadata']['KeyId'] }
     result
   end
 
-  tests("#list_keys").data_matches_schema(AWS::KMS::Formats::LIST_KEYS) do
+  tests('#list_keys').data_matches_schema(AWS::KMS::Formats::LIST_KEYS) do
     Fog::AWS[:kms].list_keys.body
   end
-
 end
