059d6489f444afe2e7cec3352b7b1fde969bbceb
geemus <geemus@gmail.com>
2024-12-09 15:51:36 +0000

    add kms test lint fixes

--- tests/requests/kms/helper.rb
+++ tests/requests/kms/helper.rb
@@ -3,25 +3,25 @@ class AWS
     module Formats
       BASIC = {
         'ResponseMetadata' => { 'RequestId' => String }
-      }
+      }.freeze
 
       DESCRIBE_KEY = {
-        "KeyMetadata" => {
-          "KeyUsage"     => String,
-          "AWSAccountId" => String,
-          "KeyId"        => String,
-          "Description"  => Fog::Nullable::String,
-          "CreationDate" => Time,
-          "Arn"          => String,
-          "Enabled"      => Fog::Boolean
+        'KeyMetadata' => {
+          'KeyUsage' => String,
+          'AWSAccountId' => String,
+          'KeyId' => String,
+          'Description' => Fog::Nullable::String,
+          'CreationDate' => Time,
+          'Arn' => String,
+          'Enabled' => Fog::Boolean
         }
-      }
+      }.freeze
 
       LIST_KEYS = {
-        "Keys"      => [{ "KeyId" => String, "KeyArn" => String }],
-        "Truncated" => Fog::Boolean,
-        "Marker"    => Fog::Nullable::String
-      }
+        'Keys' => [{ 'KeyId' => String, 'KeyArn' => String }],
+        'Truncated' => Fog::Boolean,
+        'Marker' => Fog::Nullable::String
+      }.freeze
     end
   end
 end
