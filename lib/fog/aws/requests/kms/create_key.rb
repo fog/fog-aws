dd2f55425dcb36044b50638d0897b18f248dbf4b
geemus <geemus@gmail.com>
2024-12-10 15:53:38 +0000

    [kms] convert create_key to options hash instead of positional arguments

--- lib/fog/aws/requests/kms/create_key.rb
+++ lib/fog/aws/requests/kms/create_key.rb
@@ -4,19 +4,32 @@ module Fog
       class Real
         require 'fog/aws/parsers/kms/describe_key'
 
-        def create_key(policy = nil, description = nil, usage = 'ENCRYPT_DECRYPT')
+        # Create Key
+        #
+        # ==== Parameters
+        # * options<~Hash>:
+        #   * 'Description'<~String>:
+        #   * 'KeyUsage'<~String>:
+        #   * 'Policy'<~String>:
+        #   * ... (see docs from see also)
+        #
+        # === Returns
+        #
+        # ==== See Also
+        # https://docs.aws.amazon.com/kms/latest/APIReference/API_CreateKey.html
+        def create_key(*args)
+          options = Fog::AWS::KMS.parse_create_key_args(args)
           request(
             'Action' => 'CreateKey',
-            'Description' => description,
-            'KeyUsage' => usage,
-            'Policy' => policy,
             :parser => Fog::Parsers::AWS::KMS::DescribeKey.new
-          )
+          ).merge!(options)
         end
       end
 
       class Mock
-        def create_key(_policy = nil, description = nil, usage = 'ENCRYPT_DECRYPT')
+        def create_key(*args)
+          options = Fog::AWS::KMS.parse_create_key_args(args)
+
           response = Excon::Response.new
           key_id   = UUID.uuid
           key_arn  = Fog::AWS::Mock.arn("kms", self.account_id, "key/#{key_id}", @region)
@@ -26,12 +39,10 @@ module Fog
             'AWSAccountId' => self.account_id,
             'CreationDate' => Time.now.utc,
             'DeletionDate' => nil,
-            'Description' => description,
             'Enabled' => true,
             'KeyId' => key_id,
             'KeyState' => 'Enabled',
-            'KeyUsage' => usage
-          }
+          }.merge!(options)
 
           # @todo use default policy
 
@@ -39,6 +50,32 @@ module Fog
 
           response.body = { 'KeyMetadata' => key }
           response
+        end
+      end
+
+      # previous args (policy, description, usage) was deprecated in favor of a hash of options
+      def self.parse_create_key_args(args)
+        case args.size
+        when 0
+          {}
+        when 1
+          if args[0].is_a?(Hash)
+            args[0]
+          else
+            Fog::Logger.deprecation("create_key with distinct arguments is deprecated, use options hash instead [light_black](#{caller.first})[/]")
+            {
+              'Policy' => args[0]
+            }
+          end
+        when 2, 3
+          Fog::Logger.deprecation("create_key with distinct arguments is deprecated, use options hash instead [light_black](#{caller.first})[/]")
+          {
+            'Policy' => args[0],
+            'Description' => args[1],
+            'KeyUsage' => args[2] || 'ENCRYPT_DECRYPT'
+          }
+        else
+          raise "Unknown argument style: #{args.inspect}, use options hash instead."
         end
       end
     end
