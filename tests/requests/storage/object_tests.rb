2f99ab4a245aa995af5615f8515b448d0e8181e6
ankk98 <ankk98@gmail.com>
2025-05-19 23:48:23 +0530

    Update post_object_restore tests to use options hash for parameters and add additional test case for days parameter #740

--- tests/requests/storage/object_tests.rb
+++ tests/requests/storage/object_tests.rb
@@ -96,9 +96,14 @@ Shindo.tests('AWS::Storage | object requests', ['aws']) do
       Fog::Storage[:aws].post_object_restore(@directory.identity, 'fog_object')
     end
 
-    tests("#post_object_restore('#{@directory.identity}', 'fog_object', 5, 'Bulk')").succeeds do
+    tests("#post_object_restore('#{@directory.identity}', 'fog_object', :days => 5, :tier => 'Bulk')").succeeds do
       pending unless Fog.mocking?
-      Fog::Storage[:aws].post_object_restore(@directory.identity, 'fog_object', 5, 'Bulk')
+      Fog::Storage[:aws].post_object_restore(@directory.identity, 'fog_object', :days => 5, :tier => 'Bulk')
+    end
+
+    tests("#post_object_restore('#{@directory.identity}', 'fog_object', 5)").succeeds do
+      pending unless Fog.mocking?
+      Fog::Storage[:aws].post_object_restore(@directory.identity, 'fog_object', 5)
     end
 
     tests("#put_object_acl('#{@directory.identity}', 'fog_object', 'private')").succeeds do
